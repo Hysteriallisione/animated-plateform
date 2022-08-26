f0a79e8be0e6ec09b92c7807e8a112f667f3f9fc
Hysteriallisione <yangtchenheitz@yahoo.com.au>
2022-08-26 12:57:03 +0200

    V3
    new assets in scene, debug of uptate and fixedupdate on jumping control ingame.

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public float jumpSpeed ;
+    public bool jumpTouch;
+    public float jumpForce;
     public float Speed;
     private Rigidbody2D rigBod;
+    public Vector2 force;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigBod = GetComponent<Rigidbody2D>();
     }
+    private void FixedUpdate()
+    {
+
+       
+        if (jumpTouch)
+        {
+            rigBod.AddForce(Vector2.up * jumpForce * Speed);
+            jumpTouch = false;
+            //Debug.Log("JE SAUUUTE ");
+        }
+
+
+        if (this.rigBod.velocity.y < 0)
+        {
+            rigBod.gravityScale = 3;
+        }
+        else
+        {
+            rigBod.gravityScale = 1;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * Time.deltaTime * Speed);
@@ -27,9 +52,25 @@ public class player : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.Translate(Vector3.up * Time.deltaTime * jumpSpeed);
-
+            jumpTouch = true;
         }
+       
+        //if (jumpTouch)
+        //{
+            //Debug.Log("================ update : " + jumpTouch);
+        //}
+    //   else
+      //  {
+            //Debug.Log("update:" + jumpTouch);
+       //}
+
+        // if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+        //   transform.Translate(Vector2.up + Vector2.right * Time.deltaTime * Speed);
+        //}
+        //}
+
     }
-    
 }
